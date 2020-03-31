4ef341cbbb97f4294ac69f6a45eb474b8915ead8
narendra306 <narendrakudumula@gmail.com>
2020-03-31 15:12:40 +0530

    My first commit

---
+++ App.java
@@ -0,0 +1,14 @@
+package newTours.Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		System.out.println( "Version control! Test" );
+    }
+}
